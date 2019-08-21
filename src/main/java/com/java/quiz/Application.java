bbc2abc7dfb75f3ab4f0af8a5f1131fef659d238
theBestersTm <30602439+theBestersTm@users.noreply.github.com>
2019-08-22 00:11:05 +0300

    SecondRep

--- src/main/java/com/java/quiz/Application.java
+++ src/main/java/com/java/quiz/Application.java
@@ -26,10 +26,10 @@ public class Application implements CommandLineRunner {
 
         // save a couple of customers
         repository.save(new Quiz("Laptop hardware", 1));
-        repository.save(new Quiz("Sowtware enginering", 2));
+        repository.save(new Quiz("Software engineering", 2));
 
         // fetch all customers
-        System.out.println("Customers found with findAll():");
+        System.out.println("found found with findAll():");
         System.out.println("-------------------------------");
         for (Quiz quiz : repository.findAll()) {
             System.out.println(quiz);
@@ -37,11 +37,11 @@ public class Application implements CommandLineRunner {
         System.out.println();
 
         // fetch an individual customer
-        System.out.println("Customer found with findByFirstName('Alice'):");
+        System.out.println("Quiz found with findByNameQuiz('Laptop hardware'):");
         System.out.println("--------------------------------");
-        System.out.println(repository.findByNameQuiz("Alice"));
+        System.out.println(repository.findByNameQuiz("Laptop hardware"));
 
-        System.out.println("Customers found with findByLastName('Smith'):");
+        System.out.println("Quiz found with findbyTestId('2'):");
         System.out.println("--------------------------------");
         for (Quiz quiz : repository.findbyTestId(2)) {
             System.out.println(quiz);
